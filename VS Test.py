38034896a4caaea746983db5004849ab20579e4e
kotofeu51 <kotofeu51@gmail.com>
2023-04-13 11:13:33 +0400

    13.04

--- VS Test.py
+++ VS Test.py
@@ -1,1 +1,7 @@
-print("Hello world")
+n, small, large = int(input()), 1, 1
+for i in range (1, n + 1):
+#    s = int(input())
+#    #if s > large:
+#        if small > large:
+#            large, small = s, larege
+print(1)
