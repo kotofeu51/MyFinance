3b935ec407ace4cd264845af39a3570450a5ed49
kotofeu51 <kotofeu51@gmail.com>
2023-05-04 17:16:23 +0400

    04.05

--- VS Test.py
+++ VS Test.py
@@ -1,10 +1,8 @@
-a = int(input())
-for i in range(1, a + 1):
-    for x in range( 2 * i - 1):
-        if x < i:
-            print(x + 1, end= '')
-            n = 1
-        else:
-            print(x - n, end= '')
-            n += 1
-    print()
+count = 0
+for i in range(1,33):
+    for x in range(1,33):
+        for y in range(1,33):
+            for z in range(1,33):
+                if i ** 3 + x ** 3 == y ** 3 + z ** 3 and (i != z and i != y) and count != i ** 3 + x ** 3 :
+                    print(i ** 3 + x ** 3, i, x, y, z)
+                    count = i ** 3 + x ** 3
