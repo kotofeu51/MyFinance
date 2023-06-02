600459c00f46e003b78fbf5f36e7daf00374ec19
kotofeu51 <kotofeu51@gmail.com>
2023-06-02 11:49:47 +0400

    02.06

--- VS Test.py
+++ VS Test.py
@@ -1,8 +1,6 @@
-count = 0
-for i in range(1,33):
-    for x in range(1,33):
-        for y in range(1,33):
-            for z in range(1,33):
-                if i ** 3 + x ** 3 == y ** 3 + z ** 3 and (i != z and i != y) and count != i ** 3 + x ** 3 :
-                    print(i ** 3 + x ** 3, i, x, y, z)
-                    count = i ** 3 + x ** 3
+text = input()
+Text = ''
+while text != 'stop':
+    Text += text
+    text = input(, end='\n')
+print(Text)  
